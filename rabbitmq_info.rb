746ce0c362ef83ddde55b9ceab73c2ac218ff599
Jonathan Colby <jcolby@team.mobile.de>
2015-05-29 16:06:45 +0200

    with external config and netscaler address lookup

--- rabbitmq_info.rb
+++ rabbitmq_info.rb
@@ -1,11 +1,17 @@
+#! /usr/bin/env ruby
 require 'net/http'
+require 'net/ssh'
 require 'uri'
 require 'json'
 require 'resolv'
+require 'optparse'
+require 'yaml'
 
-RABBIT_HOSTS = %w(cony47-2.mobile.rz cony46-2.mobile.rz bunny46-1.mobile.rz bunny47-1.mobile.rz bunny38-1.mobile.rz cony38-3.mobile.rz)
+
+VERSION = 1.0
 CONSUMER_API_CALL = '/api/consumers'
 SEPERATOR = '==>'
+
 =begin
 #TODO
 - use erb file to output html
@@ -14,8 +20,51 @@ curl -u guest:guest http://bunny46-1:15672/api/consumers |python -m json.tool
 =end
 
 filename = "rabbitmq.txt"
+translate = false
+config = nil
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{$0} [options]"
+  opts.release =  VERSION
+  
+  opts.on("-v", "--version", "Version info") do |v|
+    puts "#{$0} version #{opts.release}"
+    exit
+  end
+  
+  opts.on("-t", "--translate", "Default: #{translate}" ) do |t|
+    translate = true
+  end
+  
+  opts.on("-c", "--config FILE", "yaml config file" ) do |c|
+    config = c
+  end
+  
+end.parse!
+  
+abort("a yaml config file is required.  See -h for help") if (config.nil?)
+  
+  abort("config file \'#{config}\' does not exist") unless (File.file?(config)) 
+  config_parsed = begin
+    YAML.load(File.open(config))
+  rescue ArgumentError, Errno::ENOENT => e
+    $stderr.puts "Exception while opening yaml config file: #{e}"
+    exit!
+  end
+  
+  config_file = Hash.new
+  begin
+    config_file = config_parsed.inject({}){|h,(k,v)| h[k.to_sym] = v; h}
+  rescue NoMethodError => e
+    $stderr.puts "error parsing configuration yaml: #{e.message}"
+  end
+
+
+#puts config_file.keys
 
 queue_consumer = Hash.new
+connection_map = Hash.new
+rabbit_hosts = config_file[:rabbit_hosts]
 
 begin
 fhandle = File.open(filename, "r")
@@ -31,17 +80,59 @@ fhandle = File.open(filename, "r")
 rescue Errno::ENOENT
 end
 
+def gen_connection(con1, con2)
+  c1 = con1.split(/\s+/)
+  c2 = con2.split(/\s+/)
+  { "srcport" => c2[1].to_i, "srcip" => c1[0] }
+end
 
-RABBIT_HOSTS.each do |rabbit|
+if (translate)
+  # Determine real IP behind a netscaler vserver
+  # http://support.citrix.com/article/CTX126853
+  config_file.each do |ns, config|
+    connection_map[ns] = Array.new()
+    Net::SSH.start(ns.to_s, config['username'], :password => config['password']) do |ssh|
+      config['vservers'].each do |vserver|
+        output = ssh.exec!("show ns connectiontable 'VSVRNAME = " + vserver + "' -detail LINK")
+        lines = output.split("\n")
+        lines.shift # Done
+        lines.pop   # Done
+        lines.shift # header
+        loop do
+          connection_map[ns].push(gen_connection(lines.shift(), lines.shift()))
+          break if lines.empty?
+        end
+      end
+    end
+  end
+end
+
+rabbit_hosts.each do |rabbit|
   
 #queue_consumer.each { |q,h|  puts "queue: #{q}, consumers: #{h}"}
 #exit!
   response_json = response_body(get("http://#{rabbit}:15672" + CONSUMER_API_CALL,"guest","guest"))
-  
   response_json.each do |c|
     queue = c[:queue][:name]
     host = c[:channel_details][:peer_host]
-     begin
+    port = c[:channel_details][:peer_port]
+    
+    if (translate) 
+      # lookup port in netscaler ouput to replace peer_host
+      config_file.each do |ns,config|
+        if config['ip']  == host
+          cons = connection_map[ns]
+          cons.each do |con|
+            if con['srcport'] == port
+              host = con['srcip']
+              break
+            end
+          end
+        end
+      end    
+    end
+    
+    begin
       hostname = Resolv.getname(host)
      rescue Resolv::ResolvError
       hostname = host
